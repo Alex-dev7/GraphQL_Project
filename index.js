933d5ea49b3de8d1f29ac82b049110ad880ded65
Alexei Rusu <rusualexei1995@gmail.com>
2023-10-27 13:34:23 -0400

    data file

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 
+//types
+import { typeDefs } from './schema'
+
 // server setup 
 const server = new ApolloServer({
     // typeDefs - - definitions of types of data
-    
+    typeDefs,
     // resolvers
+    
 })
 
 const { url } = await startStandaloneServer(server, {
